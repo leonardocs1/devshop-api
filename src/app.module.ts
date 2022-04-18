b4f25b5eb4202bad9c5cff445b82b67a2d6ffaf2
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-18 16:05:35 -0300

    Configurando TypeORM com Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { CategoryModule } from './category/category.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Category } from './category/category.entity'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:1234@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+      // entities: [Category],
+      logging: true
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql'
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common'
-import { Category } from './dto/category'
+import { Category as CategoryDTO } from './dto/category'
+import { Category } from './category.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class CategoryService {
-  async findAll(): Promise<Category[]> {
-    return [{ id: 'a', name: 'b' }]
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>
+  ) {}
+  async findAll(): Promise<CategoryDTO[]> {
+    return this.categoryRepository.find()
   }
 }
