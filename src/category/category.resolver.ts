476b49df542fad65ddfa14aaeee526c0d9e7bcc8
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-18 22:36:42 -0300

    Create category

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -1,6 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { Category } from './category.entity'
 import { CategoryService } from './category.service'
 import { CategoryPublic } from './dto/category'
+import { CategoryCreateInput } from './dto/category-create.input'
 
 @Resolver(of => CategoryPublic)
 export class CategoryResolver {
@@ -8,5 +10,15 @@ export class CategoryResolver {
   @Query(returns => [CategoryPublic], { name: 'getAllCategories' })
   async getAllCategories(): Promise<CategoryPublic[]> {
     return await this.categoryService.findAll()
+  }
+
+  @Mutation(returns => CategoryPublic, { name: 'createCategory' })
+  async createCategory(
+    @Args('input') input: CategoryCreateInput
+  ): Promise<CategoryPublic> {
+    const categoryEntity = new Category()
+    categoryEntity.name = input.name
+    categoryEntity.slug = input.slug
+    return this.categoryService.create(categoryEntity)
   }
 }
