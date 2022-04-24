1973f6a3bee5e434c2546995ad538233eec6dd0c
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-24 17:33:33 -0300

    getCategoryById e updateCategory

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -9,12 +9,25 @@ export class CategoryService {
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>
   ) {}
+
   async findAll(): Promise<Category[]> {
     return this.categoryRepository.find()
   }
 
+  async findById(id: string): Promise<Category> {
+    return this.categoryRepository.findOne(id)
+  }
+
   async create(input: Category): Promise<Category> {
     return this.categoryRepository.save(input)
+  }
+
+  async update(input: Category): Promise<Category> {
+    await this.categoryRepository.update(input.id, {
+      name: input.name,
+      slug: input.slug
+    })
+    return input
   }
 
   async delete(id: string): Promise<boolean> {
