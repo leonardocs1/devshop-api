af3171696c1d26da45c12b7e9324016cb04c1c18
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-25 17:22:15 -0300

    Atualizar e excluir produtos ​

--- src/product/product.mapper.ts
+++ src/product/product.mapper.ts
@@ -1,6 +1,7 @@
 import { Product } from './product.entity'
 import { ProductCreateInput } from './dto/product-create.input'
 import { Category } from 'src/category/category.entity'
+import { ProductUpdateInput } from './dto/product-update.input'
 
 export class ProductMapper {
   public static toEntity(input: ProductCreateInput): Product {
@@ -8,6 +9,19 @@ export class ProductMapper {
     entity.name = input.name
     entity.slug = input.slug
     entity.description = input.description
+    const category = new Category()
+    category.id = input.category
+    entity.category = category
+    return entity
+  }
+
+  public static fromUpdateToEntity(input: ProductUpdateInput): Product {
+    const entity = new Product()
+    entity.id = input.id
+    entity.name = input.name
+    entity.slug = input.slug
+    entity.description = input.description
+
     const category = new Category()
     category.id = input.category
     entity.category = category
--- src/product/product.resolver.ts
+++ src/product/product.resolver.ts
@@ -3,23 +3,39 @@ import { Resolver } from '@nestjs/graphql'
 import { ProductPublic } from './dto/product'
 import { ProductCreateInput } from './dto/product-create.input'
 import { ProductService } from './product.service'
-import { Product } from './product.entity'
-import { Category } from 'src/category/category.entity'
 import { ProductMapper } from './product.mapper'
+import { ProductUpdateInput } from './dto/product-update.input'
 
 @Resolver(of => ProductPublic)
 export class ProductResolver {
-  constructor(private readonly producService: ProductService) {}
+  constructor(private readonly productService: ProductService) {}
+
+  @Query(returns => ProductPublic, { name: 'getProductById' })
+  async getProductById(@Args('id') id: string): Promise<ProductPublic> {
+    return await this.productService.findById(id)
+  }
 
   @Query(returns => [ProductPublic], { name: 'getAllProducts' })
   async getAllProducts(): Promise<ProductPublic[]> {
-    return await this.producService.findAll()
+    return await this.productService.findAll()
   }
 
   @Mutation(returns => ProductPublic, { name: 'createProduct' })
   async createProduct(
     @Args('input') input: ProductCreateInput
   ): Promise<ProductPublic> {
-    return this.producService.create(ProductMapper.toEntity(input))
+    return this.productService.create(ProductMapper.toEntity(input))
+  }
+
+  @Mutation(returns => ProductPublic, { name: 'updateProduct' })
+  async updateProduct(
+    @Args('input') input: ProductUpdateInput
+  ): Promise<ProductPublic> {
+    return this.productService.update(ProductMapper.fromUpdateToEntity(input))
+  }
+
+  @Mutation(returns => Boolean, { name: 'deleteProduct' })
+  async deleteProduct(@Args('id') input: string): Promise<boolean> {
+    return this.productService.delete(input)
   }
 }
