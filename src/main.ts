041f77471b5d3bcd0e1fe236c09d397a6533e867
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-04-26 22:15:48 -0300

    Injeção de dependência no Class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { useContainer } from 'class-validator'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
