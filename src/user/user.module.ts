b353266a827a3175a0ba29466009faa57c6b4629
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-05-10 09:57:38 -0300

    JWT

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user.entity'
 import { UserResolver } from './user.resolver'
@@ -6,7 +8,17 @@ import { UserService } from './user.service'
 import { UserEmailIsUnique } from './validations/UserEmailIsUnique'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET')
+      })
+    })
+  ],
   providers: [UserService, UserResolver, UserEmailIsUnique]
 })
 export class UserModule {}
